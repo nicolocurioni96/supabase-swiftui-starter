f0197b1e6baf63eaaeacf6c186d3f843fb179bc9
Nicolò Curioni <102799475+nicolocurioni96@users.noreply.github.com>
2026-02-27 14:37:13 +0100

    Initial Commit

---
+++ supabase-swiftui-starter/supabase_swiftui_starterApp.swift
@@ -0,0 +1,17 @@
+//
+//  supabase_swiftui_starterApp.swift
+//  supabase-swiftui-starter
+//
+//  Created by Nicolò Curioni on 27/02/26.
+//
+
+import SwiftUI
+
+@main
+struct supabase_swiftui_starterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
